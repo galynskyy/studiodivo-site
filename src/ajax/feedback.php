d1cc363008845903ad997c1f6ab38d504378da44
Vladimir Galynsky <galynskyy@gmail.com>
2017-02-08 12:53:33 +0300

    add core

---
+++ src/ajax/feedback.php
@@ -0,0 +1,43 @@
+<?php
+	/* SQL */
+		try {
+			$conn = new PDO("mysql:host=localhost;dbname=base", "user", "password");
+			$conn->exec("set names utf8");
+		}
+		catch (PDOException $e) {
+			echo "Connection failed: " . $e->getMessage();
+		}
+
+		$query = $conn->prepare("INSERT INTO feedback (name, email, phone, time) VALUES (:name, :email, :phone, :time)");
+		$query->bindParam(":name", $_POST["name"], PDO::PARAM_STR, 18);
+		$query->bindParam(":email", $_POST["email"], PDO::PARAM_STR, 18);
+		$query->bindParam(":phone", $_POST["phone"], PDO::PARAM_STR, 18);
+		$query->bindParam(":time", date("Y-m-d H:i:s"), PDO::PARAM_STR, 18);
+		$query->execute();
+		$result = $query->fetchColumn();
+		
+		
+		if($query->rowCount() > 0)
+		{
+			$status = "Запись добавлена";
+		}
+		else
+		{
+			$status = "Запись не добавлена";
+		}
+		
+		echo json_encode(array(
+			"status" =>	$status
+		));
+	/* /SQL */
+
+	/* Telegram */
+		$TelegramKey = "key";
+		$TelegramId1 = "id1";
+		$TelegramId2 = "id2";
+		$TelegramText = "Новая заявка:%0AИмя: ".$_POST["name"]."%0AEmail: ".$_POST["email"]."%0AТелефон: ".$_POST["phone"]."%0AДата/Время: ".date("Y-m-d H:i:s");
+
+			$sendTelegram1 = file_get_contents("https://api.telegram.org/bot".$TelegramKey."/sendMessage?chat_id=".$TelegramId1."&text=".$TelegramText);
+			$sendTelegram2 = file_get_contents("https://api.telegram.org/bot".$TelegramKey."/sendMessage?chat_id=".$TelegramId2."&text=".$TelegramText);
+	/* /Telegram */
+?>
---
+++ src/scripts/dafault.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+	$(".fancybox").fancybox();
+});
+
+$(".fancybox").click(function() {
+	yaCounter42619424.reachGoal("portfolio");
+});
+
+$(".footer-social__link").click(function() {
+	yaCounter42619424.reachGoal("social");
+});
+
+$(".header-feedback__button").click(function() {
+	var height = $("body").height(); 
+	$("body").animate({"scrollTop": height}, "slow"); 
+});
+
+$(".feedback-form").on("submit", function() {
+	yaCounter42619424.reachGoal("feedback");
+
+	var name = $("#feedback__name");
+	var email = $("#feedback__email");
+	var phone = $("#feedback__phone");
+
+	if(name.val() == "") {
+		name.focus();
+	} else if (email.val() == "") {
+		email.focus();
+	} else if (phone.val() == "") {
+		phone.focus();
+	} else {
+		$.ajax({
+			type: "POST",
+			url: "../ajax/feedback.php",
+			data: {
+				"name": name.val(),
+				"email": email.val(),
+				"phone": phone.val()
+			},
+			dataType: "json",
+			success: function(data){
+				alert(data.status);
+			}
+		});
+	}
+});
